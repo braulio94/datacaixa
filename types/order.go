7f4dadcd50768ddbd2fea8f7991eb17a73989c4f
BraulioCassule <brauliocassule94@gmail.com>
2020-01-28 14:26:24 +0100

    add order type details

--- types/order.go
+++ types/order.go
@@ -1,4 +1,33 @@
 package types
 
 type Order struct {
+	HotelId            int64
+	OrderId            int64
+	PDVId              int64
+	UserId             int64
+	ChargeId           int64
+	TableId            int64
+	ClientId           int64
+	AccomodationId     int64
+	RoomId             int64
+	AccountTypeId      int64
+	EmployeeId         int64
+	OpeningDate        string
+	ClosingDate        string
+	TotalAmount        float64
+	ServiceTax         float64
+	GeneralTotalAmount float64
+	People             int64
+	Status             string
+	TableStatus        string
+	DiscountValue      string
+	AccrualValue       float64
+	Comment            string
+	Dicount            float64
+	Accrual            float64
+	DelivererId        int64
+	DeliveryStatus     string
+	DeliveryDate       string
+	DeliveryTime       string
+	Type               string
 }
