ca108d26df365300d73cad0a48f6ae0a1f6316ed
BraulioCassule <brauliocassule94@gmail.com>
2020-02-04 17:09:54 +0100

    create frontend directory

---
+++ frontend/android/app/src/main/kotlin/datacaixa/cassule/com/datacaixa/MainActivity.kt
@@ -0,0 +1,12 @@
+package datacaixa.cassule.com.datacaixa
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
