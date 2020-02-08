df0e61e9d99690593a4767c87c2bb327cd5ba36c
BraulioCassule <brauliocassule94@gmail.com>
2020-02-08 16:53:40 +0100

    setup product group dao

---
+++ frontend/lib/database/dao/product_group_dao.dart
@@ -0,0 +1,82 @@
+import 'package:datacaixa/database/dao/dao_helper.dart';
+import 'package:datacaixa/models/product_group.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:datacaixa/common/contants.dart';
+
+class ProductGroupDao implements DaoHelper {
+  Database db;
+  ProductGroupDao(this.db);
+
+  @override
+  void createTable(Database database) async {
+    await database.execute(
+        "CREATE TABLE $productGroupTable "
+            "($identifier INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "$hotelId INTEGER, "
+            "$productGroupId INTEGER, "
+            "$description TEXT, "
+            "$display TEXT, "
+            "$code INTEGER)"
+    );
+    print("CREATED PRODUCT GROUP TABLE");
+  }
+
+  @override
+  Future get(int id, DatabaseTable table) async {
+    if(table == DatabaseTable.ProductGroup){
+      List<Map> maps = await db.query(productGroupTable,
+          columns: [identifier, hotelId, productGroupId, description, display, code],
+          where: '$identifier = ?',
+          whereArgs: [id]
+      );
+      if(maps.length > 0)
+        return ProductGroup.fromMap(maps.first);
+    }
+  }
+
+  @override
+  Future<List> getAll(DatabaseTable table) async {
+    if(table == DatabaseTable.ProductGroup){
+      List<Map> maps = await db.query(productGroupTable,
+        columns: [identifier, hotelId, productGroupId, description, display, code],
+      );
+      if(maps.length > 0) {
+        return maps.map((map) => ProductGroup.fromMap(map)).toList();
+      }
+    }
+    return [];
+  }
+
+  @override
+  void insert(item) async {
+    if(item is ProductGroup){
+      item.identifier = await db.insert(productGroupTable, item.toMap());
+    }
+  }
+
+  @override
+  void insertAll(List items) {
+    if(items is List<ProductGroup>){
+      for(ProductGroup item in items){
+        insert(item);
+      }
+    }
+  }
+
+  @override
+  void update(item) async {
+    if(item is ProductGroup){
+      await db.update(productGroupTable, item.toMap(),
+          where: '$identifier = ?', whereArgs: [item.identifier]);
+    }
+  }
+
+  @override
+  void delete(item) async {
+    if(item is ProductGroup){
+      await db.delete(productGroupTable,
+          where: '$identifier = ?', whereArgs: [item.identifier]);
+    }
+  }
+
+}
