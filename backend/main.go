2308c00ec1291430ef822bda62c8b63b11f0f454
BraulioCassule <brauliocassule94@gmail.com>
2020-02-25 11:18:24 +0100

    enable CORS

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/braulio94/datacaixa/backend/controllers"
 	"github.com/braulio94/datacaixa/backend/router"
+	"github.com/rs/cors"
 	"net/http"
 )
 
 func main() {
 	core := controllers.NewCore()
 	router.RegisterRoutes(core)
-	_ = http.ListenAndServe(":9090", core.Router)
+	handler := cors.Default().Handler(core.Router)
+	_ = http.ListenAndServe(":9090", handler)
 }
